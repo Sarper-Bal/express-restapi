225d21f9e0fc3777c5750d1c4aecffa0342be363
Sarper-Bal <49680723+Sarper-Bal@users.noreply.github.com>
2023-01-15 20:58:20 +0300

    Pug ilk kullanım

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.set('view engine', 'pug')
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
+    //res.sendFile(__dirname + '/index.html')
+    res.render('index')
 })
 
 app.listen(port, () => {
